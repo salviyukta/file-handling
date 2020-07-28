e27e3a05e23961eed4a07b2255451b4fa4e71cca
salviyukta <68501995+salviyukta@users.noreply.github.com>
2020-07-29 01:21:44 +0530

    Add files via upload

--- filewriting.c
+++ filewriting.c
@@ -2,9 +2,39 @@
 #include<conio.h>
 int main()
 {
-    FILE *fp;
+	//1. Pointer to the file
+    FILE *fp,*fnew;
+    char buff[30],ch,i=0,count=0;
+    //2. Opening file through pointer in writing mode
     fp = fopen("fresh.txt","w");
-    fprintf(fp,"My Captian");
+    //3. Input from user(eg:- My Captain)
+    printf("\n\n\t\tInput : ");
+    
+    //4. Writing input to the file
+    fgets(buff, sizeof(buff), stdin);
+    fprintf(fp, "%s", buff);
+    //fputs(buff,fp);
+    //5. Closing file
     fclose(fp);
-    return 0;
+    //6. Open both files in reading(to copy from) or writing modes(to copy to)
+    fnew = fopen("new.txt","w");
+    fp = fopen("fresh.txt","r");
+    //7. Count the number of characters in file
+    while(ch!=EOF)
+    {
+    	ch = getc(fp);
+    	count++; 	
+	}
+	//8. Copy contents in other file in reverse order
+	for(i=count-3;i>=0;i--)
+	{
+		fseek(fp,i,SEEK_SET);
+		//printf("\n%d",ftell(fp));
+		ch = fgetc(fp);
+		fputc(ch,fnew);
+	}
+	//9. Close both the files
+	fclose(fp);
+	fclose(fnew);  
+	return 0;
 }
